6d758c537497dce9813ff69e2fe09002b261e2c8
Viniuau <Viniuau@users.noreply.github.com>
2018-11-27 18:24:29 -0300

    Achou que eu tinha morrido, não foi?

---
+++ math.py
@@ -0,0 +1,27 @@
+print("Digita um número ae meu consagrado")
+x = int(input("Número 1 é esse: "))
+y = int(input("Número 2 é esse: "))
+print("Prontinho, agora escolhe o que tu quer fazer com eles")
+print("1 - Somar")
+print("2 - Subtração")
+print("3 - Multiplicação")
+print("4 - Divisão")
+escolha = int(input("Seu comando: "))
+while(escolha > 0):
+    if (escolha == 1):
+        print("A soma dá", x+y, "meu amiguinho")
+        break
+    elif (escolha == 2):
+        print("A subtração dá", x-y, "meu amiguinho")
+        break
+    elif(escolha == 3):
+        print("A multiplicação dá", x*y, "meu amiguinho")
+        break
+    elif(escolha == 4):
+        print("A divisão dá", x/y, "meu amiguinho")
+        break
+    else:
+        print("opora tu digitou o número errado")
+        break
+else:
+    print("Valeu por usar meu script para suas fórmulas matemágicas kek flw flw")
