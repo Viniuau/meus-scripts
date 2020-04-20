9b0fe2965987b0bfcca35a3e12afe4a5ceffc513
Viniuau <Viniuau@users.noreply.github.com>
2020-04-20 00:15:16 -0400

    Romantic af

---
+++ proposal.py
@@ -0,0 +1,10 @@
+import string
+
+hey = list(string.ascii_lowercase)
+i = hey[8]
+have = hey[11] + hey [14] + hey[21] + hey [4]
+something_to_say = hey[24] + hey[14] + hey[20]
+
+print(i+" "+have+" "+something_to_say)
+
+# stole from /r/programmerhumor
